556706e038b8b130b9e2075292cf004acc332ead
Shahriyar Kashif <shahriyar.k@hotmail.com>
2024-07-21 13:59:17 +0500

    update comments

--- performance-benchmark.js
+++ performance-benchmark.js
@@ -36,8 +36,9 @@ const measurePerformance = (data) => {
 measurePerformance(seedData)
 
 // From the performance benchmark, the curve forms the shape of an exponential complexity for very large
-// arrays. For smaller arrays, it is almost linear. Reference, image.png added in the folder
-// Here n is the size of array and t is the time in miliseconds required for bubbleSort to execute for
-// a particular array
-// For each iteration with swaps, we iterate through the whole array again until we reach an iteration
-// with no swaps and thus we have a O(n^2) worst case time complexity
+// arrays. For smaller arrays, it is almost linear. Reference images added in the folder
+// Here n is the size of array and t is the time in miliseconds required for insertion sort to execute for
+// a particular array. Thus time complexity is O(n^2)
+// For each iteration while the unsorted half is not empty, we iterate through the whole sorted array again
+// to shift until a smaller value than target appears and then we insert. So worst case time complexity is
+// O(n^2)
